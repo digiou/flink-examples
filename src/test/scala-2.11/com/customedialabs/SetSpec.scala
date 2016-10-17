24fd89dca9b9fcfc2ecaf8aaca87014e8985aeb6
Dimitris Giouroukis <dgiouroukis@customedialabs.com>
2016-10-17 20:32:27 +0300

    Add basic sample unit test for configuration testing

---
+++ src/test/scala-2.11/com/customedialabs/SetSpec.scala
@@ -0,0 +1,25 @@
+package com.customedialabs
+
+import collection.mutable.Stack
+import org.scalatest._
+
+/**
+  * Created by digiou on 17/10/2016.
+  */
+class ExampleSpec extends FlatSpec with Matchers{
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be (2)
+    stack.pop() should be (1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[Int]
+    a [NoSuchElementException] should be thrownBy {
+      emptyStack.pop()
+    }
+  }
+}
