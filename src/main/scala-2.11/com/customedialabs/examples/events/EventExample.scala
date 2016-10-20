5fec8bd3d40299a8a47489eb2d6a7fa7e2ae2511
Dimitris Giouroukis <dgiouroukis@customedialabs.com>
2016-10-20 15:57:09 +0300

    Implement SOJO/POJO for an example eventy type

--- src/main/scala-2.11/com/customedialabs/examples/events/EventExample.scala
+++ src/main/scala-2.11/com/customedialabs/examples/events/EventExample.scala
@@ -4,5 +4,20 @@ package com.customedialabs.examples.events
   * Created by digiou on 20/10/2016.
   */
 class EventExample {
+  private[this] var mId: Int = _
+  private[this] var mValue: Int = _
+  private[this] var mText: String = _
+  private[this] var mTimestamp: Long = _
 
+  // getters
+  def id = mId
+  def value = mValue
+  def text = mText
+  def timestamp = mTimestamp
+
+  // setters
+  def id_= (v: Int) = {mId = v}
+  def value_= (v: Int) = {mValue = v}
+  def text_= (s: String) = {mText = s}
+  def timestamp_= (t: Long) = {mTimestamp = t}
 }
