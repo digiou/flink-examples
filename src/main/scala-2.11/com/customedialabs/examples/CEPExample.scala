3de11b3c4579084191f3fbe48385cdd4b9e45484
Dimitris Giouroukis <dgiouroukis@customedialabs.com>
2016-10-20 15:56:15 +0300

    Add empty CEPExample file for future use

---
+++ src/main/scala-2.11/com/customedialabs/examples/CEPExample.scala
@@ -0,0 +1,8 @@
+package com.customedialabs.examples
+
+/**
+  * Created by digiou on 19/10/2016.
+  */
+object CEPExample extends App {
+
+}
---
+++ src/main/scala-2.11/com/customedialabs/examples/events/EventExample.scala
@@ -0,0 +1,8 @@
+package com.customedialabs.examples.events
+
+/**
+  * Created by digiou on 20/10/2016.
+  */
+class EventExample {
+
+}
