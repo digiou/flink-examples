fa99f224070cb022e29e1a0aba4b30b545773a69
Dimitris Giouroukis <dgiouroukis@customedialabs.com>
2016-10-20 15:57:57 +0300

    Add sample unit test for EventExample (simple for an example)

--- src/test/scala-2.11/com/customedialabs/examples/events/EventExampleTest.scala
+++ src/test/scala-2.11/com/customedialabs/examples/events/EventExampleTest.scala
@@ -1,8 +1,32 @@
 package com.customedialabs.examples.events
 
+import org.scalatest._
+import com.customedialabs.examples.events.EventExample
 /**
   * Created by digiou on 20/10/2016.
   */
-class EventExampleTest {
+class EventExampleTest extends FlatSpec with Matchers {
+  "An EventExample" should "have working getter and setter for ID property" in {
+    val eventExample = new EventExample
+    eventExample.id_=(0)
+    eventExample.id should be (0)
+  }
 
+  it should "have working getter and setter for value property" in {
+    val eventExample = new EventExample
+    eventExample.value_=(123)
+    eventExample.value should be (123)
+  }
+
+  it should "have working getter and setter for text property" in {
+    val eventExample = new EventExample
+    eventExample.text_=("test")
+    eventExample.text should be ("test")
+  }
+
+  it should "have working getter and setter for timestamp property" in {
+    val eventExample = new EventExample
+    eventExample.timestamp_=(Long.MinValue)
+    eventExample.timestamp should be (Long.MinValue)
+  }
 }
