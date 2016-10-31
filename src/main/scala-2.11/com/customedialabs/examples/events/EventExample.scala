cf8303137fd723d384a9c65311c304895c478136
Dimitris Giouroukis <dgiouroukis@customedialabs.com>
2016-10-31 18:32:25 +0200

    Modify EventExample to comply with Avro defaults

--- src/main/scala-2.11/com/customedialabs/examples/events/EventExample.scala
+++ src/main/scala-2.11/com/customedialabs/examples/events/EventExample.scala
@@ -3,4 +3,13 @@ package com.customedialabs.examples.events
 /**
   * Created by digiou on 20/10/2016.
   */
-case class EventExample(id: Int, value: String, timestamp: Long)
+case class EventExample(id: Int, value: String, timestamp: Long) {
+
+  def this() {
+    this(0,"", 1L)
+  }
+
+  override def toString: String = {
+    s"ID:$id\tValue:$value\tTimestamp:$timestamp"
+  }
+}
